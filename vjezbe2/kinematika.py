26027d51865d64c4e0923d7433bc09e4adf5e28e
mariamaricc <mmaric2@pmfst.hr>
2023-03-24 10:58:13 +0100

    git commit

--- vjezbe2/kinematika.py
+++ vjezbe2/kinematika.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math as math
 
 def jednoliko_gibanje(F,m):
     x_liszt = []
@@ -37,3 +38,27 @@ def jednoliko_gibanje(F,m):
     plt.plot(t, a_x)
     plt.show()
 
+def kosi_hitac(v_0, theta):
+    dt = 0.1
+    t = [0]
+    x_liszt = [0]
+    v_x = [v_0 * math.cos(math.radians(theta))]
+    v_y = [0]
+    a_x = [0]
+    a_y = [0]
+
+    for i in range(10):
+        t.append(i+1)
+        vx = v_x[i]
+        v_x.append(vx)
+        x = x_liszt[i] + v_x[i]*dt
+        x_liszt.append(x)
+
+    fig = plt.figure()
+    ax = plt.axes()
+    ax.set(xlabel = 't/[s]', ylabel = 'x/[m]')
+    ax.grid()
+    plt.plot(t, x_liszt)
+    plt.show()
+
+    print(v_x,x_liszt)
--- vjezbe2/test.py
+++ vjezbe2/test.py
@@ -1,3 +1,4 @@
 import kinematika as kin
 
-kin.jednoliko_gibanje(F = 100, m = 10)
+#kin.jednoliko_gibanje(F = 100, m = 10)
+kin.kosi_hitac(v_0=10, theta=45)
