d5fc7432c11f844c5ae4af052e5cc5106d4a751b
mariamaricc <mmaric2@pmfst.hr>
2023-06-01 23:57:23 +0200

    vjezbe9

--- vjezbe9/planet.py
+++ vjezbe9/planet.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,6 +19,33 @@ class Planet:
         self.m_z = m_z
         self.m_s = m_s
         self.t_uk = 365.242*24*3600
+
+    def __move(self,dt):
+        d1 = (self.r_z - self.r_s)**2
+        d2 = (self.r_s - self.r_z)**2
+        n1 = math.sqrt(d1[0] + d1[1])
+        n2 = math.sqrt(d2[0] + d2[1])
+
+        self.t.append(self.t[-1] + dt)
+
+        self.a_z = -self.G*(self.m_s / n1**3 )*(self.r_z - self.r_s)
+        self.a_s = -self.G*(self.m_z / n2**3 )*(self.r_s - self.r_z)
+
+        self.v_z = self.v_z + self.a_z*dt
+        self.v_s = self.v_s + self.a_s*dt
+
+        self.r_z = self.r_z + self.v_z*dt
+        self.r_s = self.r_s + self.v_s*dt
+
+        self.x_z.append(self.r_z[0])
+        self.y_z.append(self.r_z[1])
+        self.x_s.append(self.r_s[0])
+        self.y_s.append(self.r_s[1])
+
+    def range(self, dt):
+        while self.t[-1] <= self.t_uk:
+            self.__move(dt)
+        return self.x_z, self.y_z, self.x_s, self.y_s
 
     def reset(self,dt):
         self.__init__(self.r_s,self.r_z,self.v_s,self.v_z, self.a_s,self.a_z,self.m_s, self.m_z)
--- vjezbe9/test.py
+++ vjezbe9/test.py
@@ -1,3 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import planet as p
 
-p1 = p.Planet(5.9742*(10**24),1.989*(10**30),6371*(10**3),695000*(10**3))
+zemlja_sunce = p.Planet(np.array([1.496*10**(11), 0]), np.array([0.1, 0.1]), np.array([0, 29783]), np.array([0, 0]), np.array([0, 0]), np.array([0, 0]), 5.9742*10**(24), 1.989*10**(30))
+x1, y1, x2, y2 = zemlja_sunce.range(100)
+
+plt.figure(figsize=(5.5, 5.5))
+plt.plot(x1, y1)
+plt.plot(x2, y2)
+plt.legend(["Zemlja", "Sunce"])
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
